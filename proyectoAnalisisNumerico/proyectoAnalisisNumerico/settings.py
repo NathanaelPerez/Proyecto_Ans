0ead870340412295b6b388673a9b7f2b6dbfb1b5
MrEkkito <88673946+MrEkkito@users.noreply.github.com>
2025-06-24 09:48:51 -0600

    Revert "Actualizaciones visuales"
    This reverts commit 2ca936d26c380c96cddf8913bc73ec015309f351.

--- proyectoAnalisisNumerico/proyectoAnalisisNumerico/settings.py
+++ proyectoAnalisisNumerico/proyectoAnalisisNumerico/settings.py
@@ -112,8 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # ======================
 # Internacionalización
 # ======================
-LANGUAGE_CODE = 'es'
-TIME_ZONE = 'America/El_Salvador'
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
